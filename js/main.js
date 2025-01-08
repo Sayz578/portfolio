fca5fffe8d23f347c51eac5cbc6602450ab5679f
Sayz578 <kamaevyaroslav8@gmail.com>
2025-01-08 20:58:27 +0200

    commit
    add slider

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ burger.addEventListener("click", function(){
     burger.classList.toggle("active")
     nav.classList.toggle("active")
 })
+
+$(document).ready(function () {
+    $('.portfolio-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+});
